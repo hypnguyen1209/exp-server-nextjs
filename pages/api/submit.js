a8c24ab75270b0968c0ee25ebde830d632f5a41f
Hiepppppppppp <hypnguyen1209@gmail.com>
2022-12-01 15:01:33 +0700

    fix location static

--- pages/api/submit.js
+++ pages/api/submit.js
@@ -1,7 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { writeFileSync, chmodSync } from 'fs'
 export default function handler(req, res) {
-    chmodSync(__dirname + '/../../../../public/log', 777)
-    writeFileSync(__dirname + '/../../../../public/log', JSON.stringify(req.query), { flag: 'w', encoding: 'utf-8' })
+    writeFileSync(__dirname + '/../../../static/log', JSON.stringify(req.query), { flag: 'w', encoding: 'utf-8' })
     res.status(200).json({ status: 'Done' })
 }
