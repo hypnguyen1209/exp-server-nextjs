be22994128f71567f29fb7cd21349aec9e73256a
Hiepppppppppp <hypnguyen1209@gmail.com>
2022-12-01 14:45:29 +0700

    fix app

--- pages/api/submit.js
+++ pages/api/submit.js
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { writeFileSync } from 'fs'
+import { writeFileSync, chmodSync } from 'fs'
 export default function handler(req, res) {
-    console.log(req.query)
+    chmodSync(__dirname + '/../../../../public/log', 777)
     writeFileSync(__dirname + '/../../../../public/log', JSON.stringify(req.query), { flag: 'w', encoding: 'utf-8' })
     res.status(200).json({ status: 'Done' })
 }
