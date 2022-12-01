3563668f102e7d8ff62326749517e7f823fd8e4f
Hiepppppppppp <hypnguyen1209@gmail.com>
2022-12-01 15:08:30 +0700

    remove comment

--- pages/api/submit.js
+++ pages/api/submit.js
@@ -1,4 +1,3 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { writeFileSync, chmodSync } from 'fs'
 export default function handler(req, res) {
     writeFileSync(__dirname + '/../../../static/log', JSON.stringify(req.query), { flag: 'w', encoding: 'utf-8' })
